21bcdafd35d4dfaf46837557d338600697863fa8
pauloalvezz <alpa98761@gmail.com>
2025-06-03 16:59:15 -0300

    Criacao da analise knn

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from service.github import obter_dados_github 
 
 app = Flask(__name__)
-usuarios = ["lucianolpsf", "fernandallobao", "jesieldossantos", "Victorrezende19", "calebegomes740", "CaioHarrys", "aucelio0", "brunofluna", "Rafael-ai13", "Xandy77" ] 
+usuarios = ["lucianolpsf", "fernandallobao", "jesieldossantos", "Victorrezende19", "calebegomes740", "CaioHarrys", "aucelio0", "brunofluna", "Rafael-ai13", "Xandy77", ] 
 
 @app.route("/")
 def home():
@@ -12,6 +12,7 @@ def home():
 @app.route("/<usuario>")
 def rota_usuario(usuario):
     return f"<h1>Rota personalizada de {usuario}</h1>"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
