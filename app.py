5b8a6a80b03986efda43a61b08ece9e66dc418cd
Luciano lopes <Lucianolpsf@gmail.com>
2025-06-05 14:24:27 -0300

    dinamizando dados de pagina

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, render_template_string
 from jinja2 import TemplateNotFound
 from service.github import obter_dados_github 
+from dados import dados_personalizados
 
 app = Flask(__name__)
 usuarios = ["lucianolpsf", "fernandallobao", "jesieldossantos", "Victorrezende19", "calebegomes740", "CaioHarrys", "aucelio0", "brunofluna", "Rafael-ai13", "Xandy77", "pauloalvezz" ] 
@@ -14,10 +15,11 @@ def home():
 
 @app.route("/<usuario>")
 def rota_usuario(usuario):
+    dados = dados_personalizados.get(usuario, {"titulo": f"Perfil de {usuario}", "conteudo": "Nenhum dado específico disponível."})
     try:
-        return render_template(f"{usuario}.html")
+        return render_template(f"{usuario}.html",  dados=dados)
     except TemplateNotFound:
-        return render_template_string(f"<h1>Rota personalizada de {usuario}</h1>") 
+        return render_template_string(f"<h1>{dados['titulo']}</h1><p>{dados['conteudo']}</p>") 
 
 
 if __name__ == '__main__':
---
+++ dados.py
@@ -0,0 +1,46 @@
+dados_personalizados = {
+    "lucianolpsf": {
+        "titulo": "Página de lucianolpsf",
+        "conteudo": []
+    },
+    "fernandallobao": {
+        "titulo": "Página de fernandallobao",
+        "conteudo": []
+    },
+    "jesieldossantos": {
+        "titulo": "Página de jesieldossantos",
+        "conteudo": []
+    },
+    "Victorrezende19": {
+        "titulo": "Página de Victorrezende19",
+        "conteudo": []
+    },
+    "calebegomes740": {
+        "titulo": "Página de calebegomes740",
+        "conteudo": []
+    },
+    "CaioHarrys": {
+        "titulo": "Página de CaioHarrys",
+        "conteudo": []
+    },
+    "aucelio0": {
+        "titulo": "Página de aucelio0",
+        "conteudo": []
+    },
+    "brunofluna": {
+        "titulo": "Página de brunofluna",
+        "conteudo": []
+    },
+    "Rafael-ai13": {
+        "titulo": "Página de Rafael-ai13",
+        "conteudo": []
+    },
+    "Xandy77": {
+        "titulo": "Página de Xandy77",
+        "conteudo": []
+    },
+    "pauloalvezz": {
+        "titulo": "Página de pauloalvezz",
+        "conteudo": []
+    }
+}
